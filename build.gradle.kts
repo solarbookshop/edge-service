edb34dce56a23560d52c60675b761802839eba21
kartikdhiman <59189590+kartikdhiman@users.noreply.github.com>
2025-11-30 11:02:32 +0530

    feat: initialize api gateway, and basic configuration files

---
+++ build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    java
+    id("org.springframework.boot") version "4.0.0"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.solarbookshop"
+version = "0.0.1-SNAPSHOT"
+description = "API gateway and cross cutting concerns"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(25)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+extra["springCloudVersion"] = "2025.1.0"
+
+dependencies {
+    implementation("org.springframework.cloud:spring-cloud-starter-gateway-server-webflux")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("io.projectreactor:reactor-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
